da0a3005b61331cdcfa1583b920d30ad19fe435e
Andikaim <andikagamers50@gmail.com>
2021-11-30 10:58:45 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light">
             <div class="container">
                 <a class="navbar-brand font-weight-bold font-italic" href="{{ url('/') }}">
-                    {{ config('', 'Perpus') }}
+                    {{ config('', 'Absensi') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -34,21 +34,51 @@
                     <!-- Left Side Of Navbar -->
 
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a  href="{{route('home')}}" class="nav-link" > Home </a>
-                        </li>
-
 
                         <li class="nav-item">
-                            <a href="{{route('user')}}" class="nav-link" >User</a>
+                            <a href="{{route('users')}}" class="nav-link" >User</a>
                         </li>
 
                     </ul>
 
-        <main class="">
-            @include('layouts.nav')
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
             @yield('content')
-        </main>
-    </div>
+        </main>    </div>
 </body>
 </html>
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+@csrf
+<div class="container">
+    <div class="row">
+
+    </div>
+</div>
