03cf926db100b3068430cb0efa3663dbfef936f9
Andikaim <andikagamers50@gmail.com>
2021-12-01 08:18:26 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+// Route ke home
 Route::get('home', 'HomeController@index')->name('home');
 
 Route::group(['prefix'=>'master-data'], function(){
