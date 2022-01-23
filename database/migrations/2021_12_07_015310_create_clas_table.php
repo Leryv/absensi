ddd95dd92b5671c6a559d12d3c8bc39acc70e1a7
Andikaim <andikagamers50@gmail.com>
2022-01-24 00:49:27 +0700

    Class

--- database/migrations/2021_12_07_015310_create_clas_table.php
+++ database/migrations/2021_12_07_015310_create_clas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateClassesTable extends Migration
+class CreateClasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateClassesTable extends Migration
      */
     public function up()
     {
-        Schema::create('classes', function (Blueprint $table) {
+        Schema::create('clas', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('capacity');
@@ -31,6 +31,6 @@ class CreateClassesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('classes');
+        Schema::dropIfExists('clas');
     }
 }
