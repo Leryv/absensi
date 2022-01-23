4804b636297219aab79ee84e6497943e964453dc
Andikaim <andikagamers50@gmail.com>
2022-01-24 00:49:50 +0700

    User Login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('github_id')->nullable();
             $table->string('name');
+            $table->string('nisn');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
